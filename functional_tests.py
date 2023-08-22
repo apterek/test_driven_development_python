1966b885622f7d78414b1245531697d0a5f4f3d2
apterek <danialsud@gmail.com>
2023-08-22 22:30:33 +0300

    end of chapter 4

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,8 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 
@@ -12,7 +16,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do lists", self.browser.title)
-        #self.fail("ending test!")
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "The new list element isn't appear in the table"
+        )
+        self.fail("ending test!")
 
 
 if __name__ == "__main__":
