22c7e4a19ca74d4a7e57d4339f7c63c18be946c8
apterek <danialsud@gmail.com>
2023-08-28 22:37:05 +0300

    the end of the fifth chapter

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do lists", self.browser.title)
@@ -26,17 +31,17 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        inputbox.send_keys('Make a fly out of peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(by=By.ID, value='id_list_table')
-        rows = table.find_elements(by=By.TAG_NAME, value='tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "The new list element isn't appear in the table"
-        )
-        self.fail("ending test!")
+        self.check_for_row_in_list_table('2: Make a fly out of peacock feathers')
+        #self.fail("ending test!")
 
 
 if __name__ == "__main__":
